1934cd1997826e269b61128092401b3f49a5e94c
SamSKS <samuel.meireles.15s@outlook.com>
2023-03-07 08:48:37 -0300

    add: project start

---
+++ README.md
@@ -0,0 +1,31 @@
+# Twitter Simple UI
+
+Este projeto foi criado com Vite, React e TypeScript.
+
+## Como Iniciar
+
+1. Instale os pacotes necessários:
+```
+npm install
+```
+
+2. Inicie o servidor de desenvolvimento:
+```
+npm run dev
+```
+
+3. Inicie o projeto através do link que aparecerá no seu terminal
+```
+http://localhost/port
+```
+
+## Tecnologias Utilizadas
+
+
+ - Vite: um build tool rápido e simples que permite um desenvolvimento mais rápido.
+ - React: uma biblioteca JavaScript para criar interfaces de usuário.
+ - TypeScript: um superset do JavaScript que adiciona tipagem estática ao código.
+
+## Licença
+
+Este projeto está licenciado sob a licença MIT. Consulte o arquivo [LICENSE](LICENSE.md) para obter mais informações.
---
+++ src/pages/Status.tsx
@@ -0,0 +1,64 @@
+import { PaperPlaneRight } from "phosphor-react";
+import { FormEvent, KeyboardEvent, useState } from "react";
+import { Header } from "../components/Header";
+import { Separator } from "../components/Separator";
+import { Tweet } from "../components/Tweet";
+
+import "./Status.css";
+
+export function Status() {
+  const [newAnswer, setNewAnswer] = useState("");
+  const [answers, setAnswers] = useState([
+    "Concordo...",
+    "Olha, faz sentido!",
+    "Parabéns pelo progresso.",
+  ]);
+
+  function createNewAnswer(event: FormEvent) {
+    event.preventDefault();
+
+    setAnswers([newAnswer, ...answers]);
+    setNewAnswer("");
+  }
+
+  function handleHotkeySubmit(event: KeyboardEvent) {
+    if (event.key === "Enter" && (event.ctrlKey || event.metaKey)) {
+      setAnswers([newAnswer, ...answers]);
+      setNewAnswer("");
+    }
+  }
+
+  return (
+    <main className="status">
+      <Header title="Tweet" />
+
+      <Tweet content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique officia autem libero, veritatis mollitia qui nisi eveniet cumque rerum atque, unde nobis, consectetur illo assumenda. Amet totam consequuntur repudiandae cum?" />
+
+      <Separator />
+
+      <form onSubmit={createNewAnswer} className="answer-tweet-form">
+        <label htmlFor="tweet">
+          <img src="https://github.com/samsks.png" alt="Samuel Vieira" />
+          <textarea
+            id="tweet"
+            placeholder="Tweet your answer"
+            value={newAnswer}
+            onKeyDown={handleHotkeySubmit}
+            onChange={(event) => {
+              setNewAnswer(event.target.value);
+            }}
+          />
+        </label>
+
+        <button type="submit">
+          <PaperPlaneRight />
+          <span>Answer</span>
+        </button>
+      </form>
+
+      {answers.map((answer) => {
+        return <Tweet key={answer} content={answer} />;
+      })}
+    </main>
+  );
+}
